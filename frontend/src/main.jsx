06a2164a88e1de07f2cc6ce783148881a84f6eaf
Ashis Pradhan <ashispradhan394@gmail.com>
2026-09-17 19:29:57 +0530

    Add SidebarProvider to main.jsx rendering

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext";
 import { AuthProvider } from "./context/AuthContext";
+import { SidebarProvider } from "./context/SidebarContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
       <AuthProvider>
-        <App />
+        <SidebarProvider>
+          <App />
+        </SidebarProvider>
       </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
